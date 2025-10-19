241076a30217b2d26c1dad645b7fb46596a05f2e
Nurudin Wahab Lartey <nurundin2010@gmail.com>
2025-10-19 14:54:05 +0000

    step 4

--- alx-rick-and-morty-app/src/pages/pages/_app.tsx
+++ alx-rick-and-morty-app/src/pages/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
-import client from '../graphql/apolloClient';
+import client from '../../../graphql/apolloClient';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
